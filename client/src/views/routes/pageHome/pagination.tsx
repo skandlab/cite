a336f43517b36a8888e50fb68ebfe440311bc0b4
Cloud User <probhonjonbaruah@gmail.com>
2020-04-27 15:16:00 +0000

    ui: status bar pagination

--- client/src/views/routes/pageHome/pagination.tsx
+++ client/src/views/routes/pageHome/pagination.tsx
@@ -5,41 +5,46 @@ import { InterfaceData } from ".";
 const CardsPerPage: number = 12;
 
 export function calculatePaginationItems(pageNumber: number) {
-  return { start: CardsPerPage * (pageNumber - 1) + 1, end: CardsPerPage * pageNumber };
+	return {
+		start: CardsPerPage * (pageNumber - 1) + 1,
+		end: CardsPerPage * pageNumber,
+	};
 }
 
-export function paginationData(props: { filteredData: InterfaceData[]; currentPageNumber: number }) {
-  let { start, end } = calculatePaginationItems(props.currentPageNumber);
-  return props.filteredData.slice(start - 1, end);
+export function paginationData(props: {
+	filteredData: InterfaceData[];
+	currentPageNumber: number;
+}) {
+	let { start, end } = calculatePaginationItems(props.currentPageNumber);
+	return props.filteredData.slice(start - 1, end);
 }
 
-export function paginationText(props: { currentPageNumber: number; totalItems: number }) {
-  let { start, end } = calculatePaginationItems(props.currentPageNumber);
-  end = Math.min(end, props.totalItems);
-  return (
-    <span>
-      <small>{"[ " + start + "-" + end + " ]"}</small>
-      {" / "}
-      <small>{props.totalItems}</small>
-    </span>
-  );
+export function paginationText(props: {
+	currentPageNumber: number;
+	totalItems: number;
+}) {
+	let { start, end } = calculatePaginationItems(props.currentPageNumber);
+	end = Math.min(end, props.totalItems);
+	return { start: start, end: end };
 }
 
 interface Props {
-  filteredData: InterfaceData[];
-  currentPageNumber: number;
-  handleOnPageChange: (activePageNumber: number) => void;
+	filteredData: InterfaceData[];
+	currentPageNumber: number;
+	handleOnPageChange: (activePageNumber: number) => void;
 }
 
 export const AppPagination = (props: Props) => (
-  <Container textAlign="center">
-    <Pagination
-      className="pagination"
-      pointing
-      secondary
-      defaultActivePage={1}
-      totalPages={Math.ceil(props.filteredData.length / CardsPerPage)}
-      onPageChange={(_, { activePage }) => props.handleOnPageChange(activePage as number)}
-    />
-  </Container>
+	<Container textAlign="center">
+		<Pagination
+			className="pagination"
+			pointing
+			secondary
+			defaultActivePage={1}
+			totalPages={Math.ceil(props.filteredData.length / CardsPerPage)}
+			onPageChange={(_, { activePage }) =>
+				props.handleOnPageChange(activePage as number)
+			}
+		/>
+	</Container>
 );
--- client/src/views/routes/pageHome/statusBar/statusBar.tsx
+++ client/src/views/routes/pageHome/statusBar/statusBar.tsx
@@ -1,26 +1,56 @@
 import React from "react";
-import { Menu, Statistic } from "semantic-ui-react";
+import { Menu, Statistic, Form } from "semantic-ui-react";
 import { ColorBar } from "./colorbar";
 import { InterfaceData } from "..";
 import { paginationText } from "../pagination";
 
 interface Props {
-  filteredData: InterfaceData[];
-  currentPageNumber: number;
+	filteredData: InterfaceData[];
+	currentPageNumber: number;
 }
 
-export const StatusBar = (props: Props) => (
-  <Menu className="nohover" secondary borderless>
-    <Menu.Item>
-      <Statistic text size="mini">
-        <Statistic.Label>Ligand-Receptor Combinations</Statistic.Label>
-        <Statistic.Value>{paginationText({ ...props, totalItems: props.filteredData.length })}</Statistic.Value>
-      </Statistic>
-    </Menu.Item>
-    <Menu.Menu position="right">
-      <Menu.Item>
-        <ColorBar />
-      </Menu.Item>
-    </Menu.Menu>
-  </Menu>
-);
+export const StatusBar = (props: Props) => {
+	const { start, end } = paginationText({
+		...props,
+		totalItems: props.filteredData.length,
+	});
+
+	return (
+		<Menu className="nohover" secondary borderless>
+			<Menu.Item>
+				<Form>
+					<Form.Group inline>
+						<Form.Field>
+							<label>Ligand-Receptor Combinations:</label>
+						</Form.Field>
+						<Form.Field>
+							<Statistic text size="mini">
+								<Statistic.Label>
+									<small>Total</small>
+								</Statistic.Label>
+								<Statistic.Value>
+									{props.filteredData.length}
+								</Statistic.Value>
+							</Statistic>
+						</Form.Field>
+						<Form.Field>
+							<Statistic text size="mini">
+								<Statistic.Label>
+									<small>Showing</small>
+								</Statistic.Label>
+								<Statistic.Value>
+									{start + "-" + end}
+								</Statistic.Value>
+							</Statistic>
+						</Form.Field>
+					</Form.Group>
+				</Form>
+			</Menu.Item>
+			<Menu.Menu position="right">
+				<Menu.Item>
+					<ColorBar />
+				</Menu.Item>
+			</Menu.Menu>
+		</Menu>
+	);
+};
