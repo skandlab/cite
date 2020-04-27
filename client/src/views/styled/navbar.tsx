ada3c40d4e4503511842bab273c03d60aa4b9161
Cloud User <probhonjonbaruah@gmail.com>
2020-04-27 02:32:54 +0000

    ui: updated logo

--- client/src/views/styled/navbar.tsx
+++ client/src/views/styled/navbar.tsx
@@ -2,23 +2,24 @@ import React from "react";
 import { Menu, Grid, Image } from "semantic-ui-react";
 import { URL_NAVBAR_LOGO, ROUTE_HOME, APP_TITLE } from "../../utils/constants";
 import { browserHistory } from "../../utils/browser_history";
+import { GridColumn } from "./gridColumn";
 
 export const Navbar = () => (
-  <Grid.Row centered className="menuRow">
-    <Grid.Column mobile={16} tablet={16} computer={16} largeScreen={15} widescreen={12}>
-      <Menu fluid borderless>
-        <Menu.Item>
-          <Image
-            size="tiny"
-            src={URL_NAVBAR_LOGO}
-            alt={APP_TITLE + "-logo"}
-            onClick={() => browserHistory.push(ROUTE_HOME)}
-          />
-        </Menu.Item>
-        <Menu.Item>
-          <Menu.Header as="h2">{APP_TITLE}</Menu.Header>
-        </Menu.Item>
-      </Menu>
-    </Grid.Column>
-  </Grid.Row>
+	<Grid.Row centered className="menuRow">
+		<GridColumn>
+			<Menu fluid borderless>
+				<Menu.Item>
+					<Image
+						size="mini"
+						src={URL_NAVBAR_LOGO}
+						alt={APP_TITLE + "-logo"}
+						onClick={() => browserHistory.push(ROUTE_HOME)}
+					/>
+				</Menu.Item>
+				<Menu.Item>
+					<Menu.Header as="h2">{APP_TITLE}</Menu.Header>
+				</Menu.Item>
+			</Menu>
+		</GridColumn>
+	</Grid.Row>
 );
