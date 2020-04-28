e1aabb1b829495dd36b72a55951e63cbe1f55f4c
Cloud User <probhonjonbaruah@gmail.com>
2020-04-28 03:12:30 +0000

    ui: add extra row below navbar for neg spacing

--- client/src/views/styled/navbar.tsx
+++ client/src/views/styled/navbar.tsx
@@ -15,34 +15,41 @@ interface Props {
 
 export const Navbar = (props: Props) => {
 	return (
-		<Grid.Row centered className="menuRow">
-			<GridColumn>
-				<Menu fluid borderless>
-					<Menu.Item>
-						<Image
-							size="mini"
-							src={URL_NAVBAR_LOGO}
-							alt={APP_TITLE + "-logo"}
-							onClick={() =>
-								browserHistory.push(ROUTES.Home.route)
-							}
-						/>
-					</Menu.Item>
-					<Menu.Item>
-						<Menu.Header as="h2">{APP_TITLE}</Menu.Header>
-					</Menu.Item>
-					<Menu.Menu position="right">
-						{ROUTES_ARRAY.filter((val) => val.show).map((val) => (
-							<Menu.Item
-								key={val.name}
-								name={val.name}
-								active={val.matches(props.currentUrl)}
-								onClick={() => browserHistory.push(val.route)}
+		<>
+			<Grid.Row centered className="menuRow">
+				<GridColumn>
+					<Menu fluid borderless>
+						<Menu.Item>
+							<Image
+								size="mini"
+								src={URL_NAVBAR_LOGO}
+								alt={APP_TITLE + "-logo"}
+								onClick={() =>
+									browserHistory.push(ROUTES.Home.route)
+								}
 							/>
-						))}
-					</Menu.Menu>
-				</Menu>
-			</GridColumn>
-		</Grid.Row>
+						</Menu.Item>
+						<Menu.Item>
+							<Menu.Header as="h2">{APP_TITLE}</Menu.Header>
+						</Menu.Item>
+						<Menu.Menu position="right">
+							{ROUTES_ARRAY.filter((val) => val.show).map(
+								(val) => (
+									<Menu.Item
+										key={val.name}
+										name={val.name}
+										active={val.matches(props.currentUrl)}
+										onClick={() =>
+											browserHistory.push(val.route)
+										}
+									/>
+								)
+							)}
+						</Menu.Menu>
+					</Menu>
+				</GridColumn>
+			</Grid.Row>
+			<Grid.Row />
+		</>
 	);
 };
