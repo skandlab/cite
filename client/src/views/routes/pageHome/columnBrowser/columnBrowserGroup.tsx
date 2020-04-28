21515177a94bfcd00d37c1890e36fba17c47b890
Cloud User <probhonjonbaruah@gmail.com>
2020-04-28 03:11:07 +0000

    ui: change names to tumor type & interaction type

--- client/src/views/routes/pageHome/columnBrowser/columnBrowserGroup.tsx
+++ client/src/views/routes/pageHome/columnBrowser/columnBrowserGroup.tsx
@@ -3,36 +3,43 @@ import { Card } from "semantic-ui-react";
 import { ColumnBrowser, ColumnBrowserProps } from "./columnBrowser";
 
 interface Props {
-  ligandOptions: ColumnBrowserProps[];
-  receptorOptions: ColumnBrowserProps[];
-  tumorOptions: ColumnBrowserProps[];
-  pairsOptions: ColumnBrowserProps[];
-  updateLigandOrReceptor: (ligandOptions: ColumnBrowserProps[], receptorOptions: ColumnBrowserProps[]) => void;
-  updateTumorOptions: (data: ColumnBrowserProps[]) => void;
-  updatePairsOptions: (data: ColumnBrowserProps[]) => void;
+	ligandOptions: ColumnBrowserProps[];
+	receptorOptions: ColumnBrowserProps[];
+	tumorOptions: ColumnBrowserProps[];
+	pairsOptions: ColumnBrowserProps[];
+	updateLigandOrReceptor: (
+		ligandOptions: ColumnBrowserProps[],
+		receptorOptions: ColumnBrowserProps[]
+	) => void;
+	updateTumorOptions: (data: ColumnBrowserProps[]) => void;
+	updatePairsOptions: (data: ColumnBrowserProps[]) => void;
 }
 
 export const ColumnBrowserGroup = (props: Props) => (
-  <Card.Group centered doubling stackable>
-    <ColumnBrowser
-      title="Ligand"
-      options={props.ligandOptions}
-      updateOptions={(value) => props.updateLigandOrReceptor(value, props.receptorOptions)}
-    />
-    <ColumnBrowser
-      title="Receptor"
-      options={props.receptorOptions}
-      updateOptions={(value) => props.updateLigandOrReceptor(props.ligandOptions, value)}
-    />
-    <ColumnBrowser
-      title="Pairs"
-      options={props.pairsOptions}
-      updateOptions={(value) => props.updatePairsOptions(value)}
-    />
-    <ColumnBrowser
-      title="Tumor"
-      options={props.tumorOptions}
-      updateOptions={(value) => props.updateTumorOptions(value)}
-    />
-  </Card.Group>
+	<Card.Group centered doubling stackable>
+		<ColumnBrowser
+			title="Ligand"
+			options={props.ligandOptions}
+			updateOptions={(value) =>
+				props.updateLigandOrReceptor(value, props.receptorOptions)
+			}
+		/>
+		<ColumnBrowser
+			title="Receptor"
+			options={props.receptorOptions}
+			updateOptions={(value) =>
+				props.updateLigandOrReceptor(props.ligandOptions, value)
+			}
+		/>
+		<ColumnBrowser
+			title="Interaction type"
+			options={props.pairsOptions}
+			updateOptions={(value) => props.updatePairsOptions(value)}
+		/>
+		<ColumnBrowser
+			title="Tumor type"
+			options={props.tumorOptions}
+			updateOptions={(value) => props.updateTumorOptions(value)}
+		/>
+	</Card.Group>
 );
