3a85f0a63a340943d206d0d74c365a2da0758cdf
Cloud User <probhonjonbaruah@gmail.com>
2020-05-25 07:25:27 +0000

    ui: changed website title

--- client/src/utils/constants.tsx
+++ client/src/utils/constants.tsx
@@ -1,4 +1,6 @@
-export const APP_TITLE = "TUMERIC";
+export const APP_TITLE = "SITE";
+export const APP_DESCRIPTION =
+	"Signaling Interactions within Tumor microEnvironment";
 
 export const URL_NAVBAR_LOGO = require("../assets/logo.svg");
 
