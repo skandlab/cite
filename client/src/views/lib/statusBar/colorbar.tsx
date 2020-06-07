1a47ac647c197155b60ad69ada4e7e6d0f512d42
Cloud User <probhonjonbaruah@gmail.com>
2020-06-07 10:46:22 +0000

    ui: changed folder

--- client/src/views/lib/statusBar/colorbar.tsx
+++ client/src/views/lib/statusBar/colorbar.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { PlotHeatMap } from "../../../plots/plotHeatmap";
+import { PlotHeatMap } from "../../plots/plotHeatmap";
 import styled from "@emotion/styled";
 
 const data = [
