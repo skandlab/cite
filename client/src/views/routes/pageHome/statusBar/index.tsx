8b0f394d9749e60687760a0ee905d21708257c48
Cloud User <probhonjonbaruah@gmail.com>
2020-06-07 10:44:10 +0000

    ui: destructured index to summary and index

--- client/src/views/routes/pageHome/statusBar/index.tsx
+++ client/src/views/routes/pageHome/statusBar/index.tsx
@@ -1,52 +1,22 @@
 import React from "react";
-import { Menu, Statistic, Form, Card } from "semantic-ui-react";
+import { Menu } from "semantic-ui-react";
 import { ColorBar } from "./colorbar";
-import { calculatePaginationItems } from "../pagination";
+import { Summary } from "./summary";
 
 interface Props {
 	totalItems: number;
 	currentPageNumber: number;
 }
 
-export const StatusBar = (props: Props) => {
-	let { start, end } = calculatePaginationItems(props.currentPageNumber);
-	end = Math.min(end, props.totalItems);
-	start = props.totalItems === 0 ? 0 : start + 1;
-
-	return (
-		<Menu className="nohover" secondary borderless>
+export const StatusBar = (props: Props) => (
+	<Menu className="nohover" secondary borderless>
+		<Menu.Item>
+			<Summary {...props} />
+		</Menu.Item>
+		<Menu.Menu position="right">
 			<Menu.Item>
-				<Card>
-					<Card.Content textAlign="center">
-						<Form widths="equal">
-							<Form.Group inline>
-								<Form.Field>
-									<Statistic
-										label={<small>Total</small>}
-										value={props.totalItems}
-										size="mini"
-									/>
-								</Form.Field>
-								<Form.Field>
-									<Statistic
-										label={<small>Showing</small>}
-										value={start + "-" + end}
-										size="mini"
-									/>
-								</Form.Field>
-							</Form.Group>
-							<Form.Field>
-								<label>Ligand-Receptor Combinations</label>
-							</Form.Field>
-						</Form>
-					</Card.Content>
-				</Card>
+				<ColorBar />
 			</Menu.Item>
-			<Menu.Menu position="right">
-				<Menu.Item>
-					<ColorBar />
-				</Menu.Item>
-			</Menu.Menu>
-		</Menu>
-	);
-};
+		</Menu.Menu>
+	</Menu>
+);
---
+++ client/src/views/routes/pageHome/statusBar/summary.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Card, Form, Statistic } from "semantic-ui-react";
+import { calculatePaginationItems } from "../pagination";
+
+interface Props {
+	totalItems: number;
+	currentPageNumber: number;
+}
+
+export const Summary = ({ totalItems, currentPageNumber }: Props) => {
+	let { start, end } = calculatePaginationItems(currentPageNumber);
+	end = Math.min(end, totalItems);
+	start = totalItems === 0 ? 0 : start + 1;
+
+	return (
+		<Card>
+			<Card.Content textAlign="center">
+				<Form widths="equal">
+					<Form.Group inline>
+						<Form.Field>
+							<Statistic
+								label={<small>Total</small>}
+								value={totalItems}
+								size="mini"
+							/>
+						</Form.Field>
+						<Form.Field>
+							<Statistic
+								label={<small>Showing</small>}
+								value={start + "-" + end}
+								size="mini"
+							/>
+						</Form.Field>
+					</Form.Group>
+					<Form.Field>
+						<label>Ligand-Receptor Combinations</label>
+					</Form.Field>
+				</Form>
+			</Card.Content>
+		</Card>
+	);
+};
