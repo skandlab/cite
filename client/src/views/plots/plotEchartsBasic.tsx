f68041a52c96210324fab507bb2cdcbb174dd894
Cloud User <probhonjonbaruah@gmail.com>
2020-06-13 09:49:41 +0000

    ui: cleanup comments

--- client/src/views/plots/plotEchartsBasic.tsx
+++ client/src/views/plots/plotEchartsBasic.tsx
@@ -1,7 +1,6 @@
 import styled from "@emotion/styled";
 import ReactEcharts from "echarts-for-react";
 
-// TODO: check from cigrx is it is necessary
 export const PlotEchartsBasic = styled(ReactEcharts)`
 	height: 400px !important;
 	max-width: 600px;
