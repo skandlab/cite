bdb49c7853b236eecbb19d1f87c69614bbc86f18
Cloud User <probhonjonbaruah@gmail.com>
2020-06-15 08:35:31 +0000

    ui: cleanup, import was wrong

--- client/src/views/routes/pageExpression.test.tsx
+++ client/src/views/routes/pageExpression.test.tsx
@@ -4,7 +4,7 @@ import "@testing-library/jest-dom/extend-expect";
 import { render, screen, waitFor } from "@testing-library/react";
 import { Router, Route } from "react-router-dom";
 import { createMemoryHistory } from "history";
-import { ExpressionPage } from "./pageExpression";
+import ExpressionPage from "./pageExpression";
 import { ROUTES } from "../../utils/routes";
 
 describe("snapshot tests", () => {
