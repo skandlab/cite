c99f216094355ecf2870377bac66869b454ad948
Cloud User <probhonjonbaruah@gmail.com>
2020-06-17 06:33:46 +0000

    ui: improved about text copied style from medium.com max-width to 992px

--- client/src/views/routes/pageAbout.tsx
+++ client/src/views/routes/pageAbout.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "@emotion/styled";
 import {
 	Grid,
 	Card,
@@ -7,6 +8,9 @@ import {
 	Label,
 	Segment,
 	Header,
+	Item,
+	Container,
+	Divider,
 } from "semantic-ui-react";
 import { GridColumn } from "../containers/gridColumn";
 import { Members } from "../../utils/constants";
@@ -44,52 +48,66 @@ const MemberInfo = (props: Props) => (
 
 const PageAbout = () => (
 	<>
-		<Grid.Row>
-			<GridColumn>
-				<Image
-					src={require("../../assets/about_signaling.png")}
-					alt="cell-cell signaling interface"
-					size="huge"
-					centered
-				/>
-				<Segment padded="very">
-					<p>
-						<b>CITE</b> is a freely available database of curated
-						receptor-ligand signaling interactions within the tumor
-						microenvironment (TME). The cancer and stromal cell
-						specific expression was calculated by deconvolution of
-						bulk tumor expression obtained from TCGA.
-					</p>
-					<ul>
-						<li>
-							Crosstalk between ligands and receptors on cancer
-							and stromal cells was estimated in the tumor
-							microenvironment of 20 solid tumor types, based on
-							the cancer and stroma specific expression.
-						</li>
-						<li>
-							To estimate the relative flow of signaling between
-							cancer and stromal cell compartments, the Relative
-							Crosstalk (RC) score was used. Ligand-receptor
-							complex activity was approximated using the product
-							of ligand and receptor gene expression inferred for
-							the given compartments (in linear scale).
-						</li>
-						<li>
-							The RC score then estimates the relative complex
-							concentration given all four possible directions of
-							signaling and a normal tissue state, e.g. for
-							cancer-cancer (CC) signaling:
-							<Image
-								src={require("../../assets/about_formula.png")}
-								alt="relative cross-talk formula"
-								size="huge"
-								centered
-							/>
-						</li>
-					</ul>
-				</Segment>
-			</GridColumn>
+		<Grid.Row centered>
+			<StyledGridColumn>
+				<Container>
+					<em>
+						<big>
+							CITE provides a database and dynamic visualization
+							of ligand-receptor (LR) signaling interactions
+							within the tumor microenvironment (TME).
+						</big>
+					</em>
+					<Divider />
+					<Item.Group textAlign="left">
+						<StyledItem>
+							<Item.Content>
+								Crosstalk between ligands and receptors on
+								cancer and stromal cells were estimated in the
+								tumor microenvironment of 20 solid tumor types,
+								based on cancer and stroma specific expression.
+								<Image
+									src={require("../../assets/about_deconvolution.png")}
+									alt="deconvolution explanation"
+									size="huge"
+									centered
+								/>
+							</Item.Content>
+						</StyledItem>
+						<StyledItem>
+							<Item.Content>
+								The RC score then estimates the relative complex
+								concentration given all four possible directions
+								of signaling and a normal tissue state, e.g. for
+								cancer-cancer (CC) signaling:
+								<Image
+									src={require("../../assets/about_formula.png")}
+									alt="relative cross-talk formula"
+									size="huge"
+									centered
+								/>
+							</Item.Content>
+						</StyledItem>
+						<StyledItem>
+							<Item.Content>
+								To estimate the relative flow of signaling
+								between cancer and stromal cell compartments,
+								the Relative Crosstalk (RC) score was used.
+								Ligand-receptor complex activity was
+								approximated using the product of ligand and
+								receptor gene expression inferred for the given
+								compartments (in linear scale).
+								<Image
+									src={require("../../assets/about_signaling.png")}
+									alt="relative cross-talk example"
+									size="huge"
+									centered
+								/>
+							</Item.Content>
+						</StyledItem>
+					</Item.Group>
+				</Container>
+			</StyledGridColumn>
 		</Grid.Row>
 		<Grid.Row centered>
 			<GridColumn>
@@ -105,5 +123,17 @@ const PageAbout = () => (
 		</Grid.Row>
 	</>
 );
+
+const StyledGridColumn = styled(GridColumn)`
+	max-width: 992px;
+	font-size: 18px;
+`;
+
+const StyledItem = styled(Item)`
+	margin: 3em 0 !important;
+	& img {
+		margin: 1em auto !important;
+	}
+`;
 
 export default PageAbout;
